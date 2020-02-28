ea426b600e6c036f5fa3c5d63db7d8ab587b0377
Student <example@example.com>
2020-02-28 10:54:01 -0800

    add logic for multiple orders

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace Bakery
 			int OrderTotal = 0;
 			// List<Item> Items = new List<Item>();
 			Console.WriteLine("Welcome to the bakery!");
+			PlaceOrder(OrderTotal);
+		}
+		public static void PlaceOrder(int OrderTotal)
+		{
 			Console.WriteLine("Would you like to buy a loaf of bread or pastry? (Press 1 or 2)");
 			Console.WriteLine("1) Bread $2.00   2) Pastry $3.50"); 
 			int orderType = int.Parse(Console.ReadLine());
@@ -24,6 +28,7 @@ namespace Bakery
 				int numOfLoaves = int.Parse(Console.ReadLine());
 				int BreadTotal = Bread.AddBread(numOfLoaves);
 				Console.WriteLine("Order Total: $" + BreadTotal +".00");
+				ReOrder(BreadTotal);
 			} 
 			else if (number == 2)
 			{
@@ -31,7 +36,22 @@ namespace Bakery
 				int numOfPastries = int.Parse(Console.ReadLine());
 				int PastryTotal = Pastry.AddPastry(numOfPastries);
 				Console.WriteLine("Order Total: $" + PastryTotal +".00");
+				ReOrder(PastryTotal);
 			}
+		}
+		public static void ReOrder(int currentTotal)
+		{
+			Console.WriteLine("Would you like to reorder?");
+			Console.WriteLine("Y or N?");
+			string answer = Console.ReadLine();
+			if (answer.Equals("Y"))
+			{
+				PlaceOrder(currentTotal);
+			}
+			else {
+				Console.WriteLine("Thank you! Come again.");
+			}
+
 		}
 	}
 }
