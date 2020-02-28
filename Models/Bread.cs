c752407162aa5eb46581fec85622db5a863493c5
Student <example@example.com>
2020-02-28 10:33:01 -0800

    add back end logic to account for bread and pastry pricing

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -2,10 +2,22 @@ namespace Bakery.Models
 {
 	public class Bread 
 	{
-		public int Price {get;} = 2;
+		public int Price {get;} = 5;
 		public static int AddBread(int number)
 		{
-			return number *= 2;
+			int breadTotal = 0;
+			for(int i=1; i <= number; i++)
+			{
+				if(i%3 != 0)
+				{
+					breadTotal += 5;
+				} 
+				else 
+				{
+					breadTotal += 0;
+				}
+			}
+			return breadTotal;
 		}
 	}
 }
--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -2,10 +2,22 @@ namespace Bakery.Models
 {
 	public class Pastry 
 	{
-		public int Price {get;} = 3;
+		public int Price {get;} = 2;
 		public static int AddPastry(int number)
 		{
-			return number *= 3;
+			int pastryTotal = 0;
+			for(int i=1; i <= number; i++)
+			{
+				if(i%3 == 0)
+				{
+					pastryTotal += 1;
+				} 
+				else 
+				{
+					pastryTotal += 2;
+				}
+			}
+			return pastryTotal;
 		}
 	}
 }
